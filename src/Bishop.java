d152fc8adc808972c39a556b70b03ea4b9a478bf
Julien Castillon <julien.castillon@epita.fr>
2019-03-15 14:02:50 +0100

    [FIX] check Moves (Bishop)

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,2 +1,64 @@
-public class Bishop {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bishop extends Piece {
+
+    Bishop(final int x, final int y, final Alliance color){
+        super(x, y, color);
+    }
+
+    @Override
+    public List<Move> checkMoves(Board board) {
+        List<Move> moves = new ArrayList<>();
+        // TODO : factorize
+        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i++, j++) {
+            final Tile destination = board.getTile(i, j);
+            if (destination.isEmpty()) {
+                moves.add(new Move());
+            }
+            else {
+                final Piece p = destination.getPiece();
+                if (p.getAlliance() != this.color)
+                    moves.add(new Move());
+                break;
+            }
+        }
+        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i--, j++) {
+            final Tile destination = board.getTile(i, j);
+            if (destination.isEmpty()) {
+                moves.add(new Move());
+            }
+            else {
+                final Piece p = destination.getPiece();
+                if (p.getAlliance() != this.color)
+                    moves.add(new Move());
+                break;
+            }
+        }
+        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i++, j--) {
+            final Tile destination = board.getTile(i, j);
+            if (destination.isEmpty()) {
+                moves.add(new Move());
+            }
+            else {
+                final Piece p = destination.getPiece();
+                if (p.getAlliance() != this.color)
+                    moves.add(new Move());
+                break;
+            }
+        }
+        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i--, j--) {
+            final Tile destination = board.getTile(i, j);
+            if (destination.isEmpty()) {
+                moves.add(new Move());
+            }
+            else {
+                final Piece p = destination.getPiece();
+                if (p.getAlliance() != this.color)
+                    moves.add(new Move());
+                break;
+            }
+        }
+        return moves;
+    }
 }
