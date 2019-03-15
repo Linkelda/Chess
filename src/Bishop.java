fae07ff52017f78254a73e10e39cdf5541a1819b
Julien Castillon <julien.castillon@epita.fr>
2019-03-15 20:11:12 +0100

    [FIX] Factorize code

--- src/Bishop.java
+++ src/Bishop.java
@@ -1,62 +1,39 @@
+import javafx.util.Pair;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece {
 
+    private final List<Pair<Integer, Integer>> possibilities;
+
     Bishop(final int x, final int y, final Alliance color){
         super(x, y, color);
+        possibilities = new ArrayList<>();
+        possibilities.add(new Pair(-1, -1));
+        possibilities.add(new Pair(-1, 1));
+        possibilities.add(new Pair(1, -1));
+        possibilities.add(new Pair(1, 1));
     }
 
     @Override
     public List<Move> checkMoves(Board board) {
         List<Move> moves = new ArrayList<>();
-        // TODO : factorize
-        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i++, j++) {
-            final Tile destination = board.getTile(i, j);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
+        for (Pair<Integer, Integer> possibility : possibilities) {
+            int x_ = possibility.getKey() + x;
+            int y_ = possibility.getValue() + y;
+            while (Tile.isValidCoordinate(x_, y_))
+            {
+                final Tile destination = board.getTile(x_, y_);
+                if (destination.isEmpty()) {
                     moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i--, j++) {
-            final Tile destination = board.getTile(i, j);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i++, j--) {
-            final Tile destination = board.getTile(i, j);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = x, j = y; Tile.isValidCoordinate(i , j); i--, j--) {
-            final Tile destination = board.getTile(i, j);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
+                }
+                else {
+                    final Piece p = destination.getPiece();
+                    if (p.getAlliance() != this.color)
+                        moves.add(new Move());
+                    break;
+                }
             }
         }
         return moves;
--- src/Rook.java
+++ src/Rook.java
@@ -5,59 +5,34 @@ import java.util.List;
 
 public class Rook extends Piece {
 
+    private final List<Pair<Integer, Integer>> possibilities;
+
     Rook(final int x, final int y, final Alliance color){
         super(x, y, color);
+        possibilities = new ArrayList<>();
+        possibilities.add(new Pair(-1, 0));
+        possibilities.add(new Pair(1, 0));
+        possibilities.add(new Pair(0, -1));
+        possibilities.add(new Pair(0, 1));
     }
     @Override
     public List<Move> checkMoves(Board board) {
         List<Move> moves = new ArrayList<>();
-        // TODO : factorize
-        for (int i = x; Tile.isValidCoordinate(i , y); i++) {
-            final Tile destination = board.getTile(i, y);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
+        for (Pair<Integer, Integer> possibility : possibilities) {
+            int x_ = possibility.getKey() + x;
+            int y_ = possibility.getValue() + y;
+            while (Tile.isValidCoordinate(x_, y_))
+            {
+                final Tile destination = board.getTile(x_, y_);
+                if (destination.isEmpty()) {
                     moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = x; Tile.isValidCoordinate(i , y); i--) {
-            final Tile destination = board.getTile(i, y);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = y; Tile.isValidCoordinate(x , i); i++) {
-            final Tile destination = board.getTile(x, i);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
-            }
-        }
-        for (int i = y; Tile.isValidCoordinate(x , i); i--) {
-            final Tile destination = board.getTile(x, i);
-            if (destination.isEmpty()) {
-                moves.add(new Move());
-            }
-            else {
-                final Piece p = destination.getPiece();
-                if (p.getAlliance() != this.color)
-                    moves.add(new Move());
-                break;
+                }
+                else {
+                    final Piece p = destination.getPiece();
+                    if (p.getAlliance() != this.color)
+                        moves.add(new Move());
+                    break;
+                }
             }
         }
         return moves;
