8599a409c3f58fba9943cf9d585eef2765d75790
Julien Castillon <julien.castillon@epita.fr>
2019-03-18 18:31:22 +0100

    [ADD] Chess class (main class)

---
+++ src/Chess.java
@@ -0,0 +1,8 @@
+import board.Board;
+
+public class Chess {
+    public static void main(String[] args) {
+        Board board = Board.createNewBoard();
+        System.out.println(board);
+    }
+}
